a0cf81a4d71fe481b352f19959eff18a584355a9
Anton Beski <antbsk0@gmail.com>
2025-12-19 17:56:43 +0000

    ANTON BESKI (_for element h2_)

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -6,7 +6,7 @@ export default function DashboardPage() {
   return (
     <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
       <h2 className="text-3xl font-bold tracking-tight font-headline">
-        Dashboard
+        ANTON BESKI
       </h2>
       <div className="space-y-4">
         <StatsCards />
