51e7a075fdf242f5a55c7979558c7428c36f3f88
Anton Beski <antbsk0@gmail.com>
2025-12-19 17:54:00 +0000

    make all changes to completely deploy in vercel

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,6 +1,12 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
+let apiURL = 'http://localhost:9002/api/genkit';
+if (process.env.VERCEL_URL) {
+  // Set the API URL to the Vercel deployment URL
+  apiURL = `https://${process.env.VERCEL_URL}/api/genkit`;
+}
+
 export const ai = genkit({
   plugins: [googleAI()],
   model: 'googleai/gemini-2.5-flash',
