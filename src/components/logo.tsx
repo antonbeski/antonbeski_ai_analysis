89abf164c7d92377d5e3133faa187303517fde37
Anton Beski <antbsk0@gmail.com>
2025-12-19 17:35:48 +0000

    Anton Beski (_for element <Logo>_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("font-headline text-lg font-semibold", className)}>
-      antonbeski
+      Anton Beski
     </div>
   );
 }
